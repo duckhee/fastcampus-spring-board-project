435b1a36d70daa3d8a1f1357fb79d9e16b185be1
douckhee <fain9301@yahoo.com>
2022-07-24 12:13:26 +0900

    #5-Board Domain Class Set

--- src/test/java/kr/co/won/domain/ArticleCommentDomain.java
+++ src/test/java/kr/co/won/domain/ArticleCommentDomain.java
@@ -1,2 +1,22 @@
-package kr.co.won.domain;public class ArticleCommentDomain {
+package kr.co.won.domain;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class ArticleCommentDomain implements Serializable {
+
+    private Long id;
+
+    private Long articleId;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
+
 }
