1312150e7a8a10e8a947911a4526001ebca10170
douckhee <fain9301@yahoo.com>
2022-07-26 02:02:53 +0900

    #1 - 스프링 data REST -  hal JSON 추가
    API 를 쉽게 설계하기 위한 값정의

--- src/main/java/kr/co/won/repository/ArticleCommentRepository.java
+++ src/main/java/kr/co/won/repository/ArticleCommentRepository.java
@@ -2,8 +2,10 @@ package kr.co.won.repository;
 
 import kr.co.won.domain.ArticleCommentDomain;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RepositoryRestResource // 자동으로 REST API 를 만들어주는 annotation
 public interface ArticleCommentRepository extends JpaRepository<ArticleCommentDomain, Long> {
 }
