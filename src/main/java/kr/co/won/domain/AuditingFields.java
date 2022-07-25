c065de92abc3db9173abe8abdcd5a11bf36494ed
douckhee <fain9301@yahoo.com>
2022-07-26 01:26:10 +0900

    #11- AuditingFields 추출
    생성자, 생성일시, 수정자, 수정 일시 등 공통인 부분을 @MappedSuperclass를 활용하여 중복 제거

---
+++ src/main/java/kr/co/won/domain/AuditingFields.java
@@ -0,0 +1,42 @@
+package kr.co.won.domain;
+
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@EntityListeners(AuditingEntityListener.class) // 자동으로 생성을 해주기 위한 설정
+@MappedSuperclass
+public class AuditingFields {
+
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt; // 생성 일시
+
+    @CreatedBy
+    @Column(name = "created_by", nullable = false, length = 100, updatable = false)
+    private String createdBy; // 생성자
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt; // 수정 일시
+
+    @LastModifiedBy
+    @Column(name = "updated_by", nullable = false, length = 100)
+    private String updatedBy; // 수정자
+
+}
