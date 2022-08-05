5c61f122f8e9ef271c2714091c7a4faddbae0e4f
DoukheeWon <fain9301@yahoo.com>
2022-08-05 22:52:04 +0900

    #15 - controller 수정
    현재 리스트 페이지와 상세보기 페이지를 찾아갈 수 있게 컨트롤러 작성 진행

--- src/main/java/kr/co/won/controller/ArticleController.java
+++ src/main/java/kr/co/won/controller/ArticleController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,6 +24,13 @@ public class ArticleController {
     public String articles(ModelMap modelMap) {
         modelMap.addAttribute("articles", List.of());
         return "articles/index";
+    }
+
+    @GetMapping(path = "/{articleId}")
+    public String article(@PathVariable(name = "articleId") Long id, ModelMap modelMap) {
+        modelMap.addAttribute("article", null);
+        modelMap.addAttribute("articleComments", List.of());
+        return "articles/detail";
     }
 
 }
