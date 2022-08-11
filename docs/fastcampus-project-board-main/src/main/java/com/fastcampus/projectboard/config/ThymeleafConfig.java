95082011490e36052a5d9de597baf6751d7d8cbf
DoukheeWon <fain9301@yahoo.com>
2022-08-11 23:47:27 +0900

    #15 - view create design
    article 페이지의 디자인 작업을 진행

--- docs/fastcampus-project-board-main/src/main/java/com/fastcampus/projectboard/config/ThymeleafConfig.java
+++ docs/fastcampus-project-board-main/src/main/java/com/fastcampus/projectboard/config/ThymeleafConfig.java
@@ -1,17 +1,15 @@
-package kr.co.won.config;
+package com.fastcampus.projectboard.config;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 
 @Configuration
-public class ThymeleafConfiguration {
-
+public class ThymeleafConfig {
 
     @Bean
     public SpringResourceTemplateResolver thymeleafTemplateResolver(
@@ -24,15 +22,15 @@ public class ThymeleafConfiguration {
     }
 
 
-    @Getter
     @RequiredArgsConstructor
-    @ConstructorBinding // 생성자로 값을 입력해서 불변 객체로 이용하기 위해서 생성자 바인딩을 해준다.
+    @Getter
+    @ConstructorBinding
     @ConfigurationProperties("spring.thymeleaf3")
     public static class Thymeleaf3Properties {
         /**
-         * Use Thymeleaf 3 Decoupled Logic setting
+         * Use Thymeleaf 3 Decoupled Logic
          */
         private final boolean decoupledLogic;
-
     }
+
 }
