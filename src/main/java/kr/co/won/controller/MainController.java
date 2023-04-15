8f7963dd8336edb139acabb7c514105361f6839e
DoukheeWon <fain9301@yahoo.com>
2023-04-15 18:19:12 +0900

    feat: #53-swagger-ui
    swagger ui test code

--- src/main/java/kr/co/won/controller/MainController.java
+++ src/main/java/kr/co/won/controller/MainController.java
@@ -1,14 +1,40 @@
 package kr.co.won.controller;
 
+import kr.co.won.dto.response.ArticleCommentResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 메인 컨트롤러
+ * </p>
+ * 테스트 중입니다.
+ */
 @Controller
 public class MainController {
 
     @GetMapping(path = "/")
-    public String root(){
+    public String root() {
         return "redirect:/articles";
+    }
 
+    /**
+     * 댓글 정보를 열람한다.
+     *
+     * @Param id 댓글 아이디
+     * @return 댓글 알람
+     */
+    @ResponseBody
+    @GetMapping(path = "/test-rest")
+    public ArticleCommentResponse test(Long id) {
+        return ArticleCommentResponse.of(id,
+                "content",
+                LocalDateTime.now(),
+                "tester@co.kr",
+                "terter",
+                "tester");
     }
 }
